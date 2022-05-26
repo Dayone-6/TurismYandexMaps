bc25656f10fc86fc66b40f00f2737c22b23c81cf
Dayone-6 <82435692+Dayone-6@users.noreply.github.com>
2022-05-26 12:31:18 +1000

    Delete app directory

--- app/src/main/java/com/example/turismyandexmaps/ui/tickets/Answer.java
+++
@@ -1,9 +0,0 @@
-package com.example.turismyandexmaps.ui.tickets;
-
-import java.util.ArrayList;
-
-public class Answer {
-    public boolean success;
-    public ArrayList<Ticket> data;
-    public String currency;
-}
